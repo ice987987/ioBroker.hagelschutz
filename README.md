868b810ef4ddce8a5a2160604e76c3bd9908eb0b
ice987987 <mathias.frei@gmail.com>
2022-07-17 13:48:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+![Logo](admin/hagelschutz.png)
+# ioBroker.hagelschutz
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.hagelschutz.svg)](https://www.npmjs.com/package/iobroker.hagelschutz)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.hagelschutz.svg)](https://www.npmjs.com/package/iobroker.hagelschutz)
+![Number of Installations](https://iobroker.live/badges/hagelschutz-installed.svg)
+![Current version in stable repository](https://img.shields.io/badge/stable-not%20published-%23264777)
+<!-- ![Current version in stable repository](https://iobroker.live/badges/hagelschutz-stable.svg) -->
+<!-- [![Dependency Status](https://img.shields.io/david/ice987987/iobroker.hagelschutz.svg)](https://david-dm.org/ice987987/iobroker.hagelschutz) -->
+
+[![NPM](https://nodei.co/npm/iobroker.hagelschutz.png?downloads=true)](https://nodei.co/npm/iobroker.hagelschutz/)
+
+![Test and Release](https://github.com/ice987987/ioBroker.hagelschutz/workflows/Test%20and%20Release/badge.svg)
+
+[![Donate](https://img.shields.io/badge/donate-paypal-blue?style=flat)](https://paypal.me/ice987987)
+
+## hagelschutz adapter for ioBroker
+
+This adapter fetches hail warnings from https://meteo.netitservices.com.
+
+## Installation requirements
+
+* node.js >= v14.0 is required
+* js-controller >= v3.3.19 is required
+* admin >= v5.1.28 is required
+* Access to [https://meteo.netitservices.com](https://meteo.netitservices.com) which can requested via [https://www.hagelschutz-einfach-automatisch.ch](https://www.hagelschutz-einfach-automatisch.ch/eigentuemer-verwaltungen/produkt/ich-habe-interesse.html). 
+
+## Available values (readonly)
+
+You get the following value:
+* `hailState`: `0`: no hail; `1`: hail; `2`: hail state triggered by test-alarm
+
+The hail forecast is calculated every 5 minutes. Therefore, the interval to poll the API is 120 seconds. This value provides a good balance between polling too often and not missing out on any changes in the forecast.
+
+## How to report issues and feature requests
+
+Please use [GitHub issues](https://github.com/ice987987/ioBroker.hagelschutz/issues/new/choose) and fill in the form.
+
+For issues:
+Set the adapter to debug log mode (Instances -> Expert mode -> Column Log level). Get the logfile from disk (subdirectory "log" in ioBroker installation directory and not from Admin because Admin cuts the lines). Check that there are no personal information before you publish your log.
+
+## Changelog
+
+### 0.0.1
+* (ice987) initial development
+
+## License
+MIT License
+
+Copyright (c) 2022 ice987 <mathias.frei1@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,162 @@
+'use strict';
+
+/*
+ * Created with @iobroker/create-adapter v2.1.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require('@iobroker/adapter-core');
+
+// Load your modules here, e.g.:
+const axios = require('axios').default;
+
+// variables
+const isValidUrl = /https:\/\/meteo.netitservices.com\/api\/v0\/devices\/[a-zA-Z0-9]{12}\/poll\?hwtypeId=[0-9]{1,4}/;
+
+class Hagelschutz extends utils.Adapter {
+
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'hagelschutz',
+		});
+		this.on('ready', this.onReady.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+
+		this.firstStart = true;
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		// Initialize your adapter here
+		this.log.info('starting adapter "hagelschutz"...');
+
+		// Reset the connection indicator during startup
+		this.setState('info.connection', false, true);
+
+		// The adapters config (in the instance object everything under the attribute "native") is accessible via
+		this.log.debug(`config.url: ${this.config.url}`);
+
+		// check url
+		if (!isValidUrl.test(this.config.url)) {
+			this.log.error('"Url" is not valid (allowed format: https://meteo.netitservices.com/api/v0/devices/xxxxxxxxxxxx/poll?hwtypeId=yyy) (ERR_#001)');
+			return;
+		}
+
+		this.log.debug(`The configuration has been checked successfully. Trying to connect "${this.config.url}"...`);
+
+		try {
+			// create objects
+			await this.createObjects();
+
+			// get data
+			await this.getHailData();
+			await this.getHailDatabyByIntervall();
+
+		} catch (error) {
+			this.log.error(error);
+		}
+	}
+
+	// https://github.com/ioBroker/ioBroker.docs/blob/master/docs/en/dev/objectsschema.md
+	// https://github.com/ioBroker/ioBroker/blob/master/doc/STATE_ROLES.md#state-roles
+	async createObjects() {
+		this.log.debug('[createObjects]: start objects creation...');
+
+		await this.setObjectNotExistsAsync('hailState', {
+			type: 'state',
+			common: {
+				name: 'Hail State',
+				desc: 'Hail State',
+				type: 'number',
+				role: 'value.hail',
+				states: {0: 'no hail', 1: 'hail', 2: 'hail state triggered by test-alarm'},
+				min: 0,
+				max: 2,
+				read: true,
+				write: false
+			},
+			native: {}
+		});
+
+		this.log.debug('[createObjects]: Objects created...');
+	}
+
+	async getHailData() {
+		await axios({
+			method: 'GET',
+			url: this.config.url
+		})
+			.then((response) => {
+				this.log.debug(`[getHailData]: HTTP status response: ${response.status} ${response.statusText}; config: ${JSON.stringify(response.config)}; headers: ${JSON.stringify(response.headers)}; data: ${JSON.stringify(response.data)}`);
+
+				this.setState('hailState', {val: response.data.currentState, ack: true});
+
+				if (this.firstStart === true) {
+					this.setState('info.connection', true, true);
+					this.firstStart = false;
+				}
+			})
+			.catch((error) => {
+				if (error.response) {
+					// The request was made and the server responded with a status code that falls out of the range of 2xx
+					this.log.debug(`[getHailData]: HTTP status response: ${error.response.status}; headers: ${JSON.stringify(error.response.headers)}; data: ${JSON.stringify(error.response.data)}`);
+				} else if (error.request) {
+					// The request was made but no response was received `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in node.js
+					this.log.debug(`[getHailData]: error request: ${error}`);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					this.log.debug(`[getHailData]: error message: ${error.message}`);
+				}
+				this.log.debug(`[getHailData]: error.config: ${JSON.stringify(error.config)}`);
+				this.setState('info.connection', false, true);
+			});
+	}
+
+	async getHailDatabyByIntervall() {
+		this.log.info('[getHailDatabyByIntervall]: Starting polltimer with a 120 seconds interval.');
+		try {
+			this.intervall = setInterval(async () => {
+				await this.getHailData();
+			}, 120 * 1000); // default-intervall: 120s
+		} catch (error) {
+			this.log.error(`${error}: (ERR_#002)`);
+		}
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			this.intervall && clearInterval(this.intervall);
+
+			this.setState('info.connection', false, true);
+
+			callback();
+			this.log.info('cleaned everything up... (#1)');
+		} catch (e) {
+			callback();
+			this.log.info('cleaned everything up... (#2)');
+		}
+	}
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Hagelschutz(options);
+} else {
+	// otherwise start the instance directly
+	new Hagelschutz();
+}
